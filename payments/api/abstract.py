f919ee7580e0106a18b0b83a625fd896977d4ace
Mohammed Hammoud <mohammed@iktw.se>
2015-06-11 17:57:14 +0200

    Add JSONSerializerField

---
+++ payments/api/abstract.py
@@ -0,0 +1,11 @@
+from rest_framework import serializers
+
+
+class JSONSerializerField(serializers.Field):
+    """ Serializer for JSONField -- required to make field writable"""
+
+    def to_internal_value(self, data):
+        return data
+
+    def to_representation(self, value):
+        return value
